a415efb816fc36be61a300630538b9f8f0a33746
arsalan <arsalanirfan888@gmail.com>
2019-08-06 16:11:58 +0500

    Desingning and initializing Gridfs stream

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 const express = require('express');
+const path = require('path');
+const crypto = require('crypto');
+const mongoose = require('mongoose');
+const multer = requre('multer');
+const gridFsStorage = requre('multer-gridfs-storage');
+const Stream = requre('gridfs-stream');
+const bodyParser = requre('body-parser');
+const methodOverride = requre('method-override');
 
 const app = express();
-
+//MiddleWare
+app.use(bodyParser.json());
+app.use(methodOverride('_method'));
 app.set('view engine', 'ejs');
+
+//Mongo URI
+const mongoURI =
+  'mongodb://arsalan:a1b2c3d4@ds111063.mlab.com:11063/eventonclick';
 
 app.get('/', (req, res) => {
   res.render('index');
